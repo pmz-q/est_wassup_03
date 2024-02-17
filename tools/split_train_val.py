07a0b8da2d5a6f61ed4ebbc9a16f05171d5deda2
pmz-q <sundeul.dona.kim@gmail.com>
2024-02-17 17:57:30 +0900

    added: coco cls, det & yolo cls - train/val split

--- tools/split_train_val.py
+++ tools/split_train_val.py
@@ -1,5 +1,8 @@
 import argparse
+from copy import deepcopy
+import json
 from os import listdir, makedirs
+from pycocotools.coco import COCO
 import random
 import shutil
 from typing import Literal
@@ -33,25 +36,62 @@ from typing import Literal
     /tst
 """
 
+COCO_ANNOT = {
+    "info": {
+        "description": "facial-expression-classification:",
+        "url": "https://aihub.or.kr/aihubdata/data/view.do?currMenu=115&topMenu=100&aihubDataSe=data&dataSetSn=82",
+        "version": "1.2",
+        "year": 2023,
+        "contributor": "한국과학기술원",
+        "date_created": "2023/10/10"
+    },
+    "images": [
+      # {
+      #   "id": 1, # "id" must be int >= 1
+      #   "width": 426,
+      #   "height": 640,
+      #   "file_name": "xxxxxxxxx.jpg",
+      #   "date_captured": "2013-11-15 02:41:42"
+      # }
+    ],
+    "annotations": [
+      # {
+      #     "id": 1, # "id" must be int >= 1
+      #     "category_id": 1, # "category_id" must be int >= 1
+      #     "image_id": 1, # "image_id" must be int >= 1
+      #     "bbox": [86, 65, 220, 334] # [x,y,width,height]
+      # }
+    ],
+    "categories": [
+      # {
+      #   "id": 2, # "id" must be int >= 1
+      #   "name": "happy"
+      # }
+      { "id": 1, "name": "anger" },
+      { "id": 2, "name": "anxiety" },
+      { "id": 3, "name": "embarrass" },
+      { "id": 4, "name": "happy" },
+      { "id": 5, "name": "normal" },
+      { "id": 6, "name": "pain" },
+      { "id": 7, "name": "sad" },
+    ]
+  }
+
 def copy_file(
   src_root_dir: str, dst_root_dir: str, img_name: str, 
-  mode: Literal["train", "val"], annot_format: Literal["coco", "yolo"]
+  mode: Literal["train", "val"], do_for_label: bool=False
 ):
-  if annot_format == "coco":
-    pass
-  else:
-    shutil.copy(f"{src_root_dir}/images/train/{img_name}", f"{dst_root_dir}/images/{mode}/{img_name}")
+  shutil.copy(f"{src_root_dir}/images/train/{img_name}", f"{dst_root_dir}/images/{mode}/{img_name}")
+  if do_for_label:
     shutil.copy(f"{src_root_dir}/labels/train/{img_name.split('.')[0]}.txt", f"{dst_root_dir}/labels/{mode}/{img_name.split('.')[0]}.txt")
 
 def move_file(
   src_root_dir: str, img_name: str,
-  mode: Literal["train", "val"], annot_format: Literal["coco", "yolo"]
+  mode: Literal["train", "val"], do_for_label: bool=False
 ):
   if mode == "train": return
-  if annot_format == "coco":
-    pass
-  else:
-    shutil.move(f"{src_root_dir}/images/train/{img_name}", f"{src_root_dir}/images/val/{img_name}")
+  shutil.move(f"{src_root_dir}/images/train/{img_name}", f"{src_root_dir}/images/val/{img_name}")
+  if do_for_label:
     shutil.move(f"{src_root_dir}/labels/train/{img_name.split('.')[0]}.txt", f"{src_root_dir}/labels/val/{img_name.split('.')[0]}.txt")
 
 def split_list_val_train(list_images: list, trn_ratio: float):
@@ -65,8 +105,49 @@ def split_list_val_train(list_images: list, trn_ratio: float):
   trn_images = list_images[:middle]
   val_images = list_images[middle:]
   return trn_images, val_images
-  
 
+def process_per_emotion(src_root_dir:str, dst_root_dir:str, e:str, trn_ratio:float):
+  """
+  Returns:
+      tuple: ( e_trn_images, e_val_images )
+  """
+  process_file_action = move_file
+  do_copy = src_root_dir != dst_root_dir
+  if do_copy:
+    process_file_action = copy_file
+    shutil.rmtree(dst_root_dir, ignore_errors=True)
+  
+  makedirs(f"{dst_root_dir}/images/train/{e}", exist_ok=True)
+  makedirs(f"{dst_root_dir}/images/val/{e}", exist_ok=True)
+  makedirs(f"{dst_root_dir}/labels/train/{e}", exist_ok=True)
+  makedirs(f"{dst_root_dir}/labels/val/{e}", exist_ok=True)
+  
+  e_images = listdir(f"{src_root_dir}/images/train/{e}")
+  e_trn_images, e_val_images = split_list_val_train(e_images, trn_ratio)
+  for img in e_trn_images:
+    if not do_copy: break
+    process_file_action(src_root_dir, dst_root_dir, f"{e}/{img}", "train", do_for_label=False)
+  for img in e_val_images:
+    process_file_action(src_root_dir, dst_root_dir, f"{e}/{img}", "val", do_for_label=False)
+
+  return e_trn_images, e_val_images
+
+def coco_annotation_split(annot_path:str, trn_images:list, val_images:list, val_annot:dict, trn_annot:dict):
+  coco_annot = COCO(annot_path)
+  
+  for img_id in coco_annot.getImgIds():
+    img = coco_annot.imgs[img_id]
+    ann = coco_annot.imgToAnns[img_id]
+    img_name_only = img["file_name"].split("/")[1]
+    if img_name_only in val_images:
+      val_images.remove(img_name_only)
+      val_annot["images"].append(img)
+      val_annot["annotations"].append(ann)
+    else:
+      trn_images.remove(img_name_only)
+      trn_annot["images"].append(img)
+      trn_annot["annotations"].append(ann)
+    
 def yolo_detection_split(src_root_dir: str, dst_root_dir: str, train_ratio:float):
   """
   this will reformat the source root dir if src_root_dir == dst_root_dir
@@ -85,24 +166,91 @@ def yolo_detection_split(src_root_dir: str, dst_root_dir: str, train_ratio:float
   
   list_images = listdir(f"{src_root_dir}/images/train")
   trn_images, val_images = split_list_val_train(list_images, train_ratio)
-  print(len(trn_images), len(val_images))
   for img in trn_images:
     if not do_copy: break
-    process_file_action(src_root_dir, dst_root_dir, img, "train", "yolo")
+    process_file_action(src_root_dir, dst_root_dir, img, "train", do_for_label=True)
   for img in val_images:
-    process_file_action(src_root_dir, dst_root_dir, img, "val", "yolo")
+    process_file_action(src_root_dir, dst_root_dir, img, "val", do_for_label=True)
 
-def yolo_classification_split():
-  raise NotImplementedError("yolo classification train/val split")
+def yolo_classification_split(src_root_dir: str, dst_root_dir: str, train_ratio:float):
+  """
+  this will reformat the source root dir if src_root_dir == dst_root_dir
+  if not are same, this will copy images and labels to the dst_root_dir
+  """
+  emotions = listdir(f"{src_root_dir}/images/train")
+  
+  val_annot = deepcopy(COCO_ANNOT)
+  trn_annot = deepcopy(COCO_ANNOT)
+  
+  trn_images = []
+  val_images = []
+  
+  for e in emotions:
+    e_trn_images, e_val_images = process_per_emotion(src_root_dir, dst_root_dir, e, train_ratio)
+    trn_images.extend(e_trn_images)
+    val_images.extend(e_val_images)
+  
+  coco_annotation_split(f"{src_root_dir}/labels/train/annotation.json", trn_images, val_images, val_annot, trn_annot)
+  
+  with open(f"{dst_root_dir}/labels/train/annotation.json", "w", encoding="cp949") as f:
+    json.dump(trn_annot, f)
+  
+  with open(f"{dst_root_dir}/labels/val/annotation.json", "w", encoding="cp949") as f:
+    json.dump(val_annot, f)
 
-def coco_classification_split():
-  raise NotImplementedError("coco classification train/val split")
+def coco_classification_split(src_root_dir: str, dst_root_dir: str, train_ratio:float):
+  """
+  this will reformat the source root dir if src_root_dir == dst_root_dir
+  if not are same, this will copy images and labels to the dst_root_dir
+  """
+  emotions = listdir(f"{src_root_dir}/images/train")
+  
+  val_annot = deepcopy(COCO_ANNOT)
+  trn_annot = deepcopy(COCO_ANNOT)
+  
+  trn_images = []
+  val_images = []
+  
+  for e in emotions:
+    e_trn_images, e_val_images = process_per_emotion(src_root_dir, dst_root_dir, e, train_ratio)
+    trn_images.extend(e_trn_images)
+    val_images.extend(e_val_images)
+  
+  coco_annotation_split(f"{src_root_dir}/labels/train/annotation.json", trn_images, val_images, val_annot, trn_annot)
+  
+  with open(f"{dst_root_dir}/labels/train/annotation.json", "w", encoding="cp949") as f:
+    json.dump(trn_annot, f)
+  
+  with open(f"{dst_root_dir}/labels/val/annotation.json", "w", encoding="cp949") as f:
+    json.dump(val_annot, f)
 
-def coco_detection_split():
-  raise NotImplementedError("coco detection train/val split")
+def coco_detection_split(src_root_dir: str, dst_root_dir: str, train_ratio:float):
+  """
+  this will reformat the source root dir if src_root_dir == dst_root_dir
+  if not are same, this will copy images and labels to the dst_root_dir
+  """
+  emotions = listdir(f"{src_root_dir}/images/train")
+  
+  val_annot = deepcopy(COCO_ANNOT)
+  trn_annot = deepcopy(COCO_ANNOT)
+  
+  trn_images = []
+  val_images = []
+  
+  for e in emotions:
+    e_trn_images, e_val_images = process_per_emotion(src_root_dir, dst_root_dir, e, train_ratio)
+    trn_images.extend(e_trn_images)
+    val_images.extend(e_val_images)
+  
+  coco_annotation_split(f"{src_root_dir}/labels/train/annotation.json", trn_images, val_images, val_annot, trn_annot)
+  
+  with open(f"{dst_root_dir}/labels/train/annotation.json", "w", encoding="cp949") as f:
+    json.dump(trn_annot, f)
+  
+  with open(f"{dst_root_dir}/labels/val/annotation.json", "w", encoding="cp949") as f:
+    json.dump(val_annot, f)
 
 def main(cfg):
-  print(cfg)
   annot_format = cfg.annot_format[0]
   task = cfg.task[0]
   train_ratio = cfg.train_ratio

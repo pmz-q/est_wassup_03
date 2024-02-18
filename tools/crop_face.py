295e38d8f80239b25ba208ab9e5213a42be9a9a0
Jiwoo An <giwooan0410@gmail.com>
2024-02-18 08:10:09 +0000

    fixed: PR comments reflected

--- tools/crop_face.py
+++ tools/crop_face.py
@@ -3,51 +3,49 @@ import matplotlib.pyplot as plt
 from ultralytics.utils.plotting import save_one_box
 from PIL import Image
 
-def crop_face_yolo(target_size, bbox_xyxy, origin_array, custom_padding:bool=True, yolo_padding:bool=False):
+def crop_face_yolo(target_size, bbox_xyxy, origin_array, padding_option:str="custom"):
   """Crop face using bbox information
   Args:
     target_size  (int, int): (width, height) target size of cropped image,
     bbox_results (dict): predicted bbox results for all directories
-    custom_padding (bool): if True: custom padding - PIL, add black pixels. if False: no padding, only crop (size will be different)
-    yolo_padding (bool, optional): yolo padding - expand facial area
+    origin_array (np.array): original image array
+    padding_option (str): 
+      1) custom - PIL, add black pixels. 
+      2) yolo - yolo padding - expand facial area
+      3) no_padding - only crop, no resizing (size will be different)
   """
-  if custom_padding == True: 
+  if padding_option == "custom":  # custom padding with black pixels
+    cropped_img_array = save_one_box(bbox_xyxy, origin_array, square=False, save=False)
+    face_pil = Image.fromarray(cropped_img_array)
+    original_size = face_pil.size
 
-    if yolo_padding == True:  # yolo padding - expand facial areas
-      cropped_img_array = save_one_box(bbox_xyxy, origin_array, square=True, save=False)
-      # resize 추가
-      img_pil = Image.fromarray(cropped_img_array)
-      resized_img_pil = img_pil.resize(target_size, Image.BICUBIC)
-      result_array = np.array(resized_img_pil)
-      print("yolo")
+    # Calculate the aspect ratio
+    aspect_ratio = original_size[0] / original_size[1]  # width / height
+    target_aspect_ratio = target_size[0] / target_size[1]  # width / height
 
-    else: # custom padding with black pixels
-      cropped_img_array = save_one_box(bbox_xyxy, origin_array, square=False, save=False)
-      face_pil = Image.fromarray(cropped_img_array)
-      original_size = face_pil.size
+    # Calculate the new size with padding
+    if aspect_ratio > target_aspect_ratio:
+      new_width = int(target_size[0])
+      new_height = int(target_size[0] / aspect_ratio)
+    else:
+      new_width = int(target_size[1] * aspect_ratio)
+      new_height = int(target_size[1])
 
-      # Calculate the aspect ratio
-      aspect_ratio = original_size[0] / original_size[1]  # width / height
-      target_aspect_ratio = target_size[0] / target_size[1]  # width / height
+    resized_image = face_pil.resize((new_width, new_height), Image.BICUBIC) # 1) resize
+    padded_image = Image.new("RGB", target_size, (0, 0, 0)) # 2) create an empty black canvas with target size
+    paste_position = ((target_size[0] - new_width) // 2, (target_size[1] - new_height) // 2) # 3) get a position to paste the resized image
+    padded_image.paste(resized_image, paste_position) # 4) paste image to black canvas
+    result_array = np.array(padded_image) # 5) convert to numpy array
 
-      # Calculate the new size with padding
-      if aspect_ratio > target_aspect_ratio:
-        new_width = int(target_size[0])
-        new_height = int(target_size[0] / aspect_ratio)
-      else:
-        new_width = int(target_size[1] * aspect_ratio)
-        new_height = int(target_size[1])
-
-      resized_image = face_pil.resize((new_width, new_height), Image.BICUBIC) # 1) resize
-      padded_image = Image.new("RGB", target_size, (0, 0, 0)) # 2) create an empty black canvas with target size
-      paste_position = ((target_size[0] - new_width) // 2, (target_size[1] - new_height) // 2) # 3) get a position to paste the resized image
-      padded_image.paste(resized_image, paste_position) # 4) paste image to black canvas
-      result_array = np.array(padded_image) # 5) convert to numpy array
-      print("custom")
-
+  elif padding_option == "yolo":  # yolo padding - expand facial areas
+    cropped_img_array = save_one_box(bbox_xyxy, origin_array, square=True, save=False)
+    # resize 추가
+    img_pil = Image.fromarray(cropped_img_array)
+    resized_img_pil = img_pil.resize(target_size, Image.BICUBIC)
+    result_array = np.array(resized_img_pil)
+      
   else: # no resizing, only crop
     result_array = save_one_box(bbox_xyxy, origin_array, square=False, save=False)
-    print("no resize")
     
   return result_array
   
--- tools/detect_face.py
+++ tools/detect_face.py
@@ -1,11 +1,13 @@
 import argparse
 import os
+import json
+import torch
 import numpy as np
 import matplotlib.pyplot as plt
 from crop_face import crop_face_yolo
 from ultralytics import YOLO
 
-def detect_bbox_yolo(source_root:str, save_cropped_dir:str=None, save_bbox_dir:str=None, weights_path:str=None, target_size:tuple=(224,224), custom_padding:bool=True, yolo_padding:bool=False):
+def detect_bbox_yolo(dir_option:tuple, source_root:str, save_cropped_dir:str=None, save_bbox_dir:str=None, weights_path:str=None, target_size:tuple=(224,224), padding_option:str="custom"):
     """Detect face using yolov8 pretrained, save predicted bbox information
     Args:
         source_root (str): source directory
@@ -18,9 +20,9 @@ def detect_bbox_yolo(source_root:str, save_cropped_dir:str=None, save_bbox_dir:s
     Returns: 
         all_pred_bbox_results (dict): predicted bbox results for all directories
     """
-    source_root = source_root + "/images"
-    dirs = os.listdir(source_root)
     
+    # dirs = ["train", "val", "test"]
+    dirs = dir_option
     model = YOLO(weights_path)
 
     all_pred_bbox_results = {}
@@ -32,7 +34,7 @@ def detect_bbox_yolo(source_root:str, save_cropped_dir:str=None, save_bbox_dir:s
             results = model(sources, stream=True) # YOLOv8 detection, return: predicted bbox, cropped image array
             for result in results:
                 head, tail = os.path.split(result.path)
-                pred_bbox_results[emotion] = {tail: { 
+                pred_bbox_results[emotion]= {tail: { 
                     "filename": os.path.join(head.split('/')[-1], tail), # head.split('/')[-2]: emotion, tail: filename,
                     "bbox_xywh": result.boxes.xywh,  # Boxes object for bounding box outputs
                     "bbox_xyxy": result.boxes.xyxy,  # Boxes object for cropping
@@ -48,37 +50,51 @@ def detect_bbox_yolo(source_root:str, save_cropped_dir:str=None, save_bbox_dir:s
 
                 if save_cropped_dir is not None:
                     # padding처리한 cropped 이미지 저장
-                    cropped_img_array = crop_face_yolo(target_size, result.boxes.xyxy[0], result.orig_img, custom_padding, yolo_padding)
+                    cropped_img_array = crop_face_yolo(target_size, result.boxes.xyxy[0], result.orig_img, padding_option)
+                    pred_bbox_results[emotion][tail].update({"cropped_array": cropped_img_array.tolist()})
                     save_path = os.path.join(save_cropped_dir, dir, emotion)
                     os.makedirs(save_path, exist_ok=True)
                     plt.imsave(os.path.join(save_path, tail), cropped_img_array)
 
         all_pred_bbox_results[dir] = pred_bbox_results
-        print(dir, ": finished!")
+    
     return all_pred_bbox_results
 
+def convert_to_json_serializable(obj):
+    if isinstance(obj, np.ndarray):
+        return obj.tolist()
+    elif torch.is_tensor(obj):
+        return obj.cpu().numpy().tolist()
+    else:
+        raise TypeError(f"Object of type {type(obj)} is not JSON serializable")
+
 def main(cfg):
+    dir_option = tuple(cfg.dir_option)
+    print(dir_option)
     src_data_path = cfg.src_data_path
     dst_data_path = cfg.dst_data_path
     bbox_data_path = cfg.bbox_data_path
-    weights_path = cfg.bbox_weights_path
-    target_size = cfg.target_size
-    custom_padding = cfg.custom_padding
-    yolo_padding = cfg.yolo_padding
-    detect_bbox_yolo(src_data_path, dst_data_path, bbox_data_path, target_size, custom_padding, yolo_padding)
+    weights_path = cfg.weights_path
+    target_size = tuple(cfg.target_size)
+    padding_option = cfg.padding_option
+    detect_results = detect_bbox_yolo(dir_option, src_data_path, dst_data_path, bbox_data_path, weights_path, target_size, padding_option)
+    
+    # with open(os.path.join(dst_data_path, "detect_info.json"), "w") as json_file:
+    #     json.dumps(detect_results, indent=4, default=convert_to_json_serializable)
+    # print(dir, ": finished!")
 
     
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   
-  parser.add_argument("--src-data-path", type=str, default="../data")
-  parser.add_argument("--dst-data-path", type=str, default="../cropped_data") # destination path for cropped image  
-  parser.add_argument("--bbox-data-path", type=str, default=None) # for checking bbox in orginal image
-  parser.add_argument("--weights-path", type=str, default="/home/KDT-admin/work/weights/yolov8n-face.pt") # put pretrained weights path to load
-  parser.add_argument("--target_size", type=tuple, default=(224,224)) # target cropping image size
-  parser.add_argument("--custom_padding", type=bool, default=True) # if True: custom padding - PIL, add black pixels. if False: no padding, only crop (size will be different)
-  parser.add_argument("--yolo_padding", type=bool, default=False) # expand facial area.
+  parser.add_argument("--dir-option", nargs="+", type=str, default=("train", "val", "test"), help="choose specific folders to detect&crop")
+  parser.add_argument("--src-data-path", type=str, default="../data/images", help="path where contains source images") 
+  parser.add_argument("--dst-data-path", type=str, default="../cropped_data", help="destination path for cropped image") 
+  parser.add_argument("--bbox-data-path", type=str, default=None, help="save original image with bbox printed") 
+  parser.add_argument("--weights-path", type=str, default="/home/KDT-admin/work/weights/yolov8n-face.pt", help="pretrained weights path to load") 
+  parser.add_argument("--target-size", nargs=2, type=int, default=(224, 224), help="target cropping image size, separated by space, example: 224 224")  
+  parser.add_argument("--padding-option", type=str, default="custom", choices=["custom", "yolo", "no_padding"], help="custom: resizing and add black pixels, yolo: no black pad, expand facial area to resize, no_padding: only crop, no resizing")
   config = parser.parse_args()
 
   main(config)

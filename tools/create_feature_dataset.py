4c4a41c33caf6f452344dfcd99bb2a84032f97cf
pmz-q <sundeul.dona.kim@gmail.com>
2024-02-19 14:55:46 +0900

    added: progress bar

--- tools/create_feature_dataset.py
+++ tools/create_feature_dataset.py
@@ -6,12 +6,14 @@ from os import listdir, walk, makedirs
 import shutil
 from typing import Literal
 from uuid import uuid4
+import tqdm
 
 
 def dfs(
   content:dict={"name": "root", "content": {}}, parent_path:str="../data",
   filename_old_to_new:dict={}, change_folder_name:bool=False,
-  src_root_dir:str="/home/KDT-admin/work/selected_images", dst_root_dir:str="../data", mode:Literal["train", "test"]="train"
+  src_root_dir:str="/home/KDT-admin/work/selected_images", dst_root_dir:str="../data", mode:Literal["train", "test"]="train",
+  progress_bar=tqdm.tqdm(total=len(100))
 ):
   for dir in listdir(parent_path):
     uuid = str(uuid4())
@@ -25,6 +27,7 @@ def dfs(
         # 주의!!!!
         # 이거 개발할 땐 data/images/anger... data/labels/anger... 이런식의 구조에서 짬
         filename_old_to_new[dir] = f"{parent_path.replace(f'{src_root_dir}/images/{mode}/', '')}/{uuid}"
+      progress_bar.update(1)
     else: # rename dirs
       if change_folder_name:
         content["content"][uuid] = { "name": dir, "content": {} }
@@ -42,20 +45,32 @@ def dfs(
 def create_structure_dataset(src_root_dir:str, dst_root_dir:str, filename_history:str, mode:Literal["train", "test"]="train"):
   filename_old_to_new = {}
   new_coco_annot = None
-    
-  dfs(filename_history["root"]["images"][mode], f"{src_root_dir}/images/{mode}", filename_old_to_new, False, src_root_dir, f"{dst_root_dir}/images/{mode}", mode)
-  dfs(filename_history["root"]["labels"][mode], f"{src_root_dir}/labels/{mode}", filename_old_to_new, False, src_root_dir, f"{dst_root_dir}/labels/{mode}", mode)
+  
+  # TODO: progress bar 코드 정리
+  tqdm_len = 0
+  for e in listdir(f"{src_root_dir}/images"):
+    tqdm_len += len(listdir(f"{src_root_dir}/images/{e}"))
+  
+  progress_bar = tqdm.tqdm(total=tqdm_len, desc="copy renamed images") 
+  dfs(filename_history["root"]["images"][mode], f"{src_root_dir}/images/{mode}", filename_old_to_new, False, src_root_dir, f"{dst_root_dir}/images/{mode}", mode, progress_bar)
+  progress_bar.close()
+  progress_bar = tqdm.tqdm(total=len(listdir(f"{src_root_dir}/labels")), desc="copy renamed origin annotations") 
+  dfs(filename_history["root"]["labels"][mode], f"{src_root_dir}/labels/{mode}", filename_old_to_new, False, src_root_dir, f"{dst_root_dir}/labels/{mode}", mode, progress_bar)
+  progress_bar.close()
 
   # TODO: 데이터 폴더 구조에 따라서 수정해야 할 수 있음
   # 주의!!!!
   # 이거 개발할 땐 data/images/anger... data/labels/anger... 이런식의 구조에서 짬
+  progress_bar = tqdm.tqdm(total=len(listdir(f"{src_root_dir}/labels")), desc="create coco annotations") 
   for (root, dirs, files) in walk(f"{src_root_dir}/labels/{mode}"):
       if root == f"{src_root_dir}/labels/{mode}": continue
       for file in files:
         with open(f"{root}/{file}", encoding="cp949") as f:
           origin_annots = json.load(f)
         new_coco_annot = convert_origin_to_coco(origin_annots, f"{dst_root_dir}/images/{mode}", new_coco_annot, change_img_name=True, img_names=filename_old_to_new)
-    
+        progress_bar.update(1)
+  progress_bar.close()
+  
   with open(f"{dst_root_dir}/labels/{mode}/annotation.json", "w", encoding="cp949") as f:
     json.dump(new_coco_annot, f)
 
